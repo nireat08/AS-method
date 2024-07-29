5a45e51d5def55e2c4744a8c86d4f1995cc42437
nireat08 <burin1@naver.com>
2024-07-29 23:36:36 +0900

    css구조 변경

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-let mainMenu = document.querySelectorAll('.Main_solving > li')
-let header = document.querySelector('body>header');
+// let mainMenu = document.querySelectorAll('.Main_solving > li')
+// let header = document.querySelector('body>header');
 
-mainMenu.forEach((item) =>{
-    item.addEventListener('mouseover',function() {
-        let submenuHeight = this.querySelector('.sub_menu').offsetHeight;
-        header.style.height = `${submenuHeight + 110}px`;
-    });
-    item.addEventListener('mouseout',function() {
-        header.style.height= '110px' ;
-    });
-});
+// mainMenu.forEach((item) =>{
+//     item.addEventListener('mouseover',function() {
+//         let submenuHeight = this.querySelector('.sub_menu').offsetHeight;
+//         header.style.height = `${submenuHeight + 110}px`;
+//     });
+//     item.addEventListener('mouseout',function() {
+//         header.style.height= '110px' ;
+//     });
+// });
